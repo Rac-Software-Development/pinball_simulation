691ed762e80056dd7cc31f296168e51a4cb54b09
Dylan-1024040 <1024040@hr.nl>
2024-12-09 13:21:42 +0100

    The ball is visible on the screen

--- components/ball.py
+++ components/ball.py
@@ -1,9 +1,19 @@
 import pymunk
+import pygame
 
 class Ball:
-    def __init__(self, x, y, radius):
+    def __init__(self, x, y, radius, mass):
+        self.radius = radius
         self.body = pymunk.Body(mass, pymunk.moment_for_circle(mass, 0, radius))
         self.body.position = x, y
         self.shape = pymunk.Circle(self.body, radius)
-        self.shape.elasticity = elasticity
-        self.shape.friction = friction
+        self.shape.elasticity = 0.8
+        self.shape.friction = 0.5
+        
+    def draw(self, screen):
+        pygame.draw.circle(
+            screen,
+            (128, 128, 128),
+            (int(self.body.position.x), int(self.body.position.y)),
+            self.radius
+        )
--- main.py
+++ main.py
@@ -1,67 +1,53 @@
 import pygame
 import pymunk
-import pymunk.pygame_util
+from components.ball import Ball
 
 
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 900
+FPS = 60
 
-pygame.init()
-
-WIDTH, HEIGHT = 1000, 800
-window = pygame.display.set_mode((WIDTH, HEIGHT))
-
-
-
-def draw(space, window, draw_options):
-    window.fill("white")
-    space.debug_draw(draw_options)
-    pygame.display.update()
-
-
-def create_ball(space, radius, mass):
-    body = pymunk.Body()
-    body.position = (100, 300)
-    shape = pymunk.Circle(body, radius)
-    shape.mass = mass
-    shape.color = (0, 0, 255, 255)
-    space.add(body, shape)
-    return shape
-
-def create_line(space, start, end):
-    body = pymunk.Body(body_type=pymunk.Body.STATIC)
-    shape = pymunk.Segment(body, start, end, 5)
-    shape.color = (0, 0, 0, 255)
-    space.add(body, shape)
-    return shape
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption('Dylan\'s Pinball simulation')
     
-    
-
-def run(window):
-    running = True
-    clock = pygame.time.Clock()
-    fps = 60
-    dt = 1 / fps
     
     space = pymunk.Space()
     space.gravity = (0, 900)
     
-    ball = create_ball(space, 20, 5)
+    ball = Ball(300, 100, radius=10, mass=1)
     
-    line = create_line(space, (0, 400), (WIDTH - 200, 600))
     
-    draw_options = pymunk.pygame_util.DrawOptions(window)
+    space.add(ball.body, ball.shape)
     
-    while running:
+    
+    clock = pygame.time.Clock()
+    
+    run = True
+    while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
-                break
+                run = False
                 
-        draw(space, window, draw_options)
-        space.step(dt)
-        clock.tick(fps)
+        screen.fill((0, 0, 0))
+        
+        ball.draw(screen)
+        space.step(1 / FPS)
+        
+        pygame.display.flip()
+        clock.tick(FPS)
+
         
     pygame.quit()
     
 if __name__ == "__main__":
-    run(window)
+    main()
+    
+    
+
+
+
+    
+
         
