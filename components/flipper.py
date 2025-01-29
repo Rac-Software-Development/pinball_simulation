e61d3b752fd8fd2f939aec8dd23a23847f9d4008
Dylan-1024040 <1024040@hr.nl>
2025-01-29 14:32:47 +0100

    Adjusted the flipper component

--- components/flipper.py
+++ components/flipper.py
@@ -10,20 +10,23 @@ class Flipper:
         self.angle = angle
         self.color = color
         
-        
+        # create the flipper body
         mass = 2
         moment = pymunk.moment_for_box(mass, (length, 20))
         self.body = pymunk.Body(mass, moment)
         self.body.position = self.position
         self.body.angle = self.angle
         
+        # create the flipper shape
         self.shape = pymunk.Poly.create_box(self.body, (length, 20))
         self.shape.friction = 1
         self.space.add(self.body, self.shape)
         
+        # create the pivot point
         pivot = pymunk.PivotJoint(self.space.static_body, self.body, self.position)
         self.space.add(pivot)
         
+        # Create line that is attached to the flipper
         self.line_start = self.position
         self.line_end = self.position + pymunk.Vec2d(self.length, 0).rotated(self.angle)
         
@@ -37,4 +40,4 @@ class Flipper:
         pygame.draw.line(screen, (0,0,0), self.line_start, self.line_end, 2)
 
     def update_line(self):
-        self.line_end =  self.position
+        self.line_end =  self.position + pymunk.Vec2d(self.length, 0).rotated(self.body.angle)
