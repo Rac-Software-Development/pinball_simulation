0d96e19a3095b87dd9384694144857bf29c2c0f5
Dylan-1024040 <1024040@hr.nl>
2025-02-12 14:34:20 +0100

    added some comments

--- main.py
+++ main.py
@@ -13,59 +13,63 @@ FPS = 60
 
 def create_outer_lines(space):
    return [
-       # left line
+       # left line slighly diagonal to the top center
        OuterLine(space, [
            (50, 850), # bottom left
            (50, 50), # top left
            (300, 10), # diagonal line to top center
        ], color=(255, 255, 255)),
        
-       # right line
+       # right line slighly diagonal to the top center
        OuterLine(space, [
            (550, 850), # bottom right
            (550, 50), # top right
            (300, 10) # diagonal line to top center
        ], color=(255, 255, 255)),
        
-       # bottom line
+       # bottom left section of the bottom line
        OuterLine(space, [
-           (200, 850),
-           (50, 850),
+           (200, 850), # middle bottom
+           (50, 850), # bottom left
        ], color=(255, 255, 255)),
 
-        # bottom line
+        # bottom right section of the bottom line
        OuterLine(space, [
-           (550, 850),
-           (450, 850),
+           (550, 850), # bottom right
+           (450, 850), # slightly left of the middle bottom
        ], color=(255, 255, 255)),
    ]
    
     
-
+# the main function
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Dylan's Pinball simulation")
     clock = pygame.time.Clock()
 
+    # create the space
     space = pymunk.Space()
     space.gravity = (0, 600)
     
-
+    # create the ball
     ball = Ball(space, (150, 100), radius=10, mass=2, elasticity=0.85)
     ball.body.velocity = pymunk.Vec2d(0, 50)
 
-
+    # create the outer lines
     outer_lines = create_outer_lines(space)
 
+    # create the line that guides the ball to the flippers
     ball_guides = [
         BallGuide(space, (175, 750), (100, 675)), # left guide
         BallGuide(space, (425, 750), (500, 675)) # right guide
     ]
 
+    # create the flippers
     left_flipper = Flipper(space, (230, 750), is_left=True)
     right_flipper = Flipper(space, (370, 750), is_left=False)
 
+    # create the bumpers
     bumpers = [
         Bumper(space, (200, 600), color=(255, 255, 255)),
         Bumper(space, (400, 600), color=(255, 255, 255)),
