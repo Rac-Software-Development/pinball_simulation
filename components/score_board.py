5c694d4d8daafcfaeabf5d291240e551b8bf2fe7
Dylan-1024040 <1024040@hr.nl>
2025-02-23 14:44:28 +0100

    I adjusted the score board to put in the lives as well

--- components/score_board.py
+++ components/score_board.py
@@ -1,14 +1,26 @@
 import pygame
 
 class ScoreBoard:
-    def __init__(self, font_size=30, position=(10, 10)):
+    def __init__(self, font_size=30, position=(10, 10), lives=3):
         self.score = 0
+        self.lives = lives
         self.font = pygame.font.SysFont("Arial", font_size)
         self.position = position
+
+    def draw(self, screen):
+        score_text = self.font.render(f"Score: {self.score}", True, (255,255,255))
+        lives_text = self.font.render(f"Score: {self.score}", True, (255,255,255))
+        
+        screen.blit(score_text, self.position)
+        screen.blit(lives_text, (self.position[0], self.position[1] + 40))
 
     def increase_score(self, points):
         self.score += points
 
-    def draw(self, screen):
-        score_surface = self.font.render(f"Score: {self.score}", True, (255,255,255))
-        screen.blit(score_surface, self.position)
+    def decrease_life(self):
+        self.lives -= 1
+
+    def reset(self):
+        self.score = 0
+        self.lives = 3
+        
